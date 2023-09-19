acb898bbb793a6470a96ecf99304e2b80153bf73
heesuePark <phs8743@naver.com>
2023-09-19 23:19:26 +0900

    chore:반복로직 커스텀훅으로 변경

--- mobi-path-nextjs/1week/2-nextproject/src/pages/portfolio/[pid].tsx
+++ mobi-path-nextjs/1week/2-nextproject/src/pages/portfolio/[pid].tsx
@@ -3,20 +3,12 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Post from "@/types";
 import { fetchData } from "@/api";
+import { usePost } from "@/hooks/usePost";
 
 export default function ChangeRoute() {
   const router = useRouter();
   const { pid } = router.query;
-  const [postList, setPostList] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const data: Post[] = await fetchData();
-      // pid를 string으로 인식해서 계속 빨간줄 => Number로 감싸줌
-      setPostList(data.filter((v) => v.id == Number(pid)));
-    };
-    fetchPosts();
-  }, [pid]);
+  const postList = usePost(pid);
 
   return (
     <>
